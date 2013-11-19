fdbf97c5bd9e5f58c092c7b90c51223a37d1f6ea
Kirill Smelkov <kirr@mns.spb.ru>
2013-11-19 17:34:03 +0400

    dotnet: Implement WVXFAIL and WVSKIP
    Add abilities to skip and mark test with known breakage to dotnet
    driver.
    
    The driver implements results counting, and that is extended for counting
    xfail/xpass/skip as well, although I don't get the rationale behind this
    results counting is, and should be, done in wvtestrun!
    
    Interaction with expect_fail is marked as TODO because rationale behind
    it is not well understood - if it expected to fail - that's WVFAIL, but
    there is no WVNOEXCEPT, and that expect_next_failure prefix is used. I
    decided to better do it simple first, and then, if needed, it could be
    adjusted (expect_next_failure() is unique to dotnet driver).
    
    This is my first experience with C#, so please don't beat me too much.

--- dotnet/wvtest.cs.h
+++ dotnet/wvtest.cs.h
@@ -6,4 +6,6 @@
 #define WVEXCEPT(x) { System.Exception _wvex = null; try { x; } catch (System.Exception _wvasserte) { _wvex = _wvasserte; } WvTest.test(_wvex != null, __FILE__, __LINE__, "EXCEPT(" + #x + ")"); if (_wvex != null) throw _wvex; }
 #define WVPASSEQ(x, y) try { WvTest.test_eq((x), (y), __FILE__, __LINE__, #x, #y); } catch (Wv.Test.WvAssertionFailure) { throw; } catch (System.Exception) { WvTest.test_exception(__FILE__, __LINE__, string.Format("[{0}] == [{1}]", #x, #y)); throw; }
 #define WVPASSNE(x, y) try { WvTest.test_ne((x), (y), __FILE__, __LINE__, #x, #y); } catch (Wv.Test.WvAssertionFailure) { throw; } catch (System.Exception) { WvTest.test_exception(__FILE__, __LINE__, string.Format("[{0}] != [{1}]", #x, #y)); throw; }
+#define WVXFAIL(x) try { WvTest.xtest(WvTest.booleanize(x), __FILE__, __LINE__, #x); } catch (Wv.Test.WvAssertionFailure) { throw; } catch (System.Exception) { WvTest.test_exception(__FILE__, __LINE__, #x); throw; }
+#define WVSKIP(x) WvTest.report("skip", __FILE__, __LINE__, #x)
 #endif // __WVTEST_CS_H
